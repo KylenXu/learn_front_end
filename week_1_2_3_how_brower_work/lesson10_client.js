c8c0dbcb16630529f248ea45909be0651b7d68e1
KylenXu <kylen8036@gmail.com>
2021-05-09 23:16:36 +0800

    绘制DOM树

--- week_1_2_3_how_brower_work/lesson10_client.js
+++ week_1_2_3_how_brower_work/lesson10_client.js
@@ -217,8 +217,9 @@ void async function () {
   let dom = parseHTML(response.body);
 
   let viewport = images(800, 600);
-  render(viewport, dom.children[0].children[3].children[1].children[1]);
-  render(viewport, dom.children[0].children[3].children[1].children[3]);
+  // render(viewport, dom.children[0].children[3].children[1].children[1]);
+  // render(viewport, dom.children[0].children[3].children[1].children[3]);
+  render(viewport, dom);
   viewport.save('viewport.jpg');
   // console.log(JSON.stringify(dom, null, "  "));
   // console.log('');
